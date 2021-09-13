3f2ab2f441e27156f9e07512a331cc3179714c5d
okugario <alcimic972@gmail.com>
2021-09-13 15:15:21 +0700

    Update to 1.3

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,68 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
-import { Form, Input, Button } from "antd";
+import { Input, Button } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import "./App.css";
-import { ApiFetch } from "./Helpers";
 function App() {
-  const [form] = Form.useForm();
-
   return (
-    <div className="body">
-      <form
-        form={form}
-        onFinish={() => {
-          form.submit();
-        }}
-        method="POST"
-        action="authenticate"
-        name="normal_login"
-        className="form "
-        initialValues={{
-          remember: true,
-        }}
-      >
-        <img className="form_title" src="img/logo.jpg" />
-        <Form.Item
-          name="login"
-          rules={[
-            {
-              required: false,
-              message: "Пожалуйста введите ваш логин!",
-            },
-          ]}
-        >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Логин"
-            type="text"
-            className="form_input"
-          />
-        </Form.Item>
-        <Form.Item
-          name="pass"
-          rules={[
-            {
-              required: false,
-              message: "Пожалуйста введите ваш пароль!",
-            },
-          ]}
-        >
-          <Input
-            className="form_input"
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            placeholder="Пароль"
-            type="password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="form_button">
-            Вход
-          </Button>
-        </Form.Item>
-      </form>
-    </div>
+    <form
+      method="POST"
+      action="authenticate"
+      name="normal_login"
+      className="form"
+    >
+      <img className="form_title" src="img/logo.jpg" />
+
+      <Input
+        prefix={<UserOutlined className="site-form-item-icon" />}
+        placeholder="Логин"
+        type="text"
+        name="login"
+        className="form_input"
+      />
+
+      <Input
+        className="form_input"
+        prefix={<LockOutlined className="site-form-item-icon" />}
+        placeholder="Пароль"
+        type="password"
+        name="pass"
+      />
+
+      <Button type="primary" htmlType="submit" className="form_button">
+        Вход
+      </Button>
+    </form>
   );
 }
 ReactDOM.render(<App />, document.getElementById("App"));
