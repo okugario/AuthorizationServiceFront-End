832084bd652bd261322035d96cd96747bc230e13
okugario <alcimic972@gmail.com>
2021-09-13 15:44:10 +0700

    Update to 1.4

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -6,34 +6,36 @@ import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import "./App.css";
 function App() {
   return (
-    <form
-      method="POST"
-      action="authenticate"
-      name="normal_login"
-      className="form"
-    >
-      <img className="form_title" src="img/logo.jpg" />
+    <div className="body">
+      <form
+        method="POST"
+        action="authenticate"
+        name="normal_login"
+        className="form"
+      >
+        <img className="form_title" src="img/logo.jpg" />
 
-      <Input
-        prefix={<UserOutlined className="site-form-item-icon" />}
-        placeholder="Логин"
-        type="text"
-        name="login"
-        className="form_input"
-      />
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Логин"
+          type="text"
+          name="login"
+          className="form_input"
+        />
 
-      <Input
-        className="form_input"
-        prefix={<LockOutlined className="site-form-item-icon" />}
-        placeholder="Пароль"
-        type="password"
-        name="pass"
-      />
+        <Input
+          className="form_input"
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          placeholder="Пароль"
+          type="password"
+          name="pass"
+        />
 
-      <Button type="primary" htmlType="submit" className="form_button">
-        Вход
-      </Button>
-    </form>
+        <Button type="primary" htmlType="submit" className="form_button">
+          Вход
+        </Button>
+      </form>
+    </div>
   );
 }
 ReactDOM.render(<App />, document.getElementById("App"));
